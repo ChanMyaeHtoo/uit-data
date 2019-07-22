0f4b399836536854961807e9a1f0c40ad6ba5a51
Frenco <frenco.willian@gmail.com>
2019-07-22 09:36:49 +0630

    create initial file

---
+++ papers.js
@@ -0,0 +1,279 @@
+module.exports = {
+    data: {
+      icait2017: [
+        {
+          topic: 'Cloud Computing and Big Data Analysis',
+          papers: [
+              {
+                  title: 'Dynamic Replication Management Scheme for Cloud Storage',
+                  authors: ['May Phyo Thu', 'Khine Moe New', 'Kyar Nyo Aye']
+              },
+              {
+                  title: 'Performance Analysis of a Scalable Naive Bayes Classifier on Beyond MapReduce',
+                  authors: ['Myat Cho Mon Oo', 'Thandar Thein']
+              },
+              {
+                  title: 'Cloud Based Big Data Application of FP-Growth Algorithm and K-Means\nClustering Algorithm Based on MapReduce Hadoop ',
+                  authors: ['Than Htike Aung', 'Nang Saing Moon Kham']
+              }, 
+              {
+                  title: 'Analytics of Reliability for Real-Time Big Data Pipeline Architecture',
+                  authors: ['Thandar Aung', 'Aung Htein Maw']
+              },
+              {
+                  title: 'Optimum Checkpoint Interval for MapReduce Fault-Tolerance',
+                  authors: ['Naychi Nway Nway', 'Julia Myint']
+              },
+              {
+                  title: 'Forensic Analysis of Residual Artifacts on CDH Storage',
+                  authors: ['Myat Nandar Oo', 'Thandar Thein']
+              }
+          ]
+        },
+        {
+          topic: 'Networking and Network Security',
+          papers: [
+              {
+                  title: '',
+                  authors: []
+              },
+              {
+                  title: '',
+                  authors: []
+              },
+              {
+                  title: '',
+                  authors: []
+              },
+              {
+                  title: '',
+                  authors: []
+              }
+          ]
+        },
+        {
+          topic: 'Data Science',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Natural Language Processing',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Software Engineering and Web Mining',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Image and Signal Processing',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Mobile and Dirstributed Computing',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        }
+      ],
+      icait2018: [
+        {
+          topic: 'Artificial Intelligence and Robotics',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Cloud and Distributed Computing',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Data Mining',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Database and Big Data Analysis',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Image Processing',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Natural Language Processing',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Simulation and Modeling in Software Approach',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        },
+        {
+          topic: 'Wireless Networking and Software Defined Network',
+          papers: [
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            },
+            {
+                title: '',
+                authors: []
+            }
+          ]
+        }
+      ]
+    }
+  }
